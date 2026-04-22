64b7a3b8a6ff014e92649d75da1134c0feedbc4d
“vibhurastogi98-ops” <vibhurastogi98@gmail.com>
2026-04-22 16:31:27 +0530

    deploy fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+    plugins: [react()],
     server: {
-        port: 3001,       // change to 3001
-        strictPort: true, // IMPORTANT: stop auto switching
+        port: 3001,
+        strictPort: true,
         open: true,
         proxy: {
             '/api': 'http://localhost:3002',
